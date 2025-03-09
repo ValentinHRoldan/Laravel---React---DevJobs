dcd4cf59819d12d8cf04de331971a5b21c5252d9
ValentinHRoldan <valentinroldan223@gmail.com>
2025-03-09 01:52:04 -0300

    nav arreglado

--- React-DevJobs/src/Layouts/Layout.jsx
+++ React-DevJobs/src/Layouts/Layout.jsx
@@ -4,14 +4,12 @@ import { Outlet } from 'react-router-dom'
 export default function Layout({children}) {
   return (
     <div class="min-h-screen bg-gray-100 dark:bg-gray-900 text-white">
+        {children}
         <header class="bg-white dark:bg-gray-800 shadow">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                 <Outlet/>
             </div>
         </header>
-    <main>
-        {children}
-    </main>
     </div>
   )
 }
--- React-DevJobs/src/Layouts/Navigation.jsx
+++ React-DevJobs/src/Layouts/Navigation.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Enlace from '../components/Enlace'
+import { Link } from 'react-router-dom'
 
 export default function Navigation() {
     return (
@@ -8,11 +9,11 @@ export default function Navigation() {
                 <div class="flex justify-between h-16">
                     <div class="flex">
                         <div class="shrink-0 flex items-center">
-                            <a href="">
+                            <Link to={'/'}>
                                 <p className='block h-9 text-3xl w-auto fill-current text-gray-800 dark:text-gray-200'>
                                     Dev<span class="font-extrabold">Jobs</span> 
                                 </p>
-                            </a>
+                            </Link>
                         </div>
                         <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                             <a href="" className='inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 dark:border-indigo-600 text-sm font-medium leading-5 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'>Mis Vacantes</a>
